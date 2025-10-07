31ccd1bc3bf35f6f6f17dde0d9a834a57ecc04d3
VictoriaO26 <yvictoria2020@gmail.com>
2025-10-07 06:35:23 -0400

    My progress before week 1 updates

---
+++ src/components/UploadProgressSim.tsx
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+export const UploadProgressSim = () => {
+
+    const [progress, setProgress] = useState(0);
+    const [isUploading, setIsUploading] = useState(false);
+    const [uploadId, setuploadID] = useState(null);
+
+    //event handlers
+
+    //TODO
+
+    //TODO: Stop upload in progress
+    
+    return (
+        <div>
+
+        </div>
+    );
+
+};
+
+export default UploadProgressSim;
